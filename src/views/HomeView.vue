a38c25059b7189ae4c676e5a0f96845d9a52f7c8
Thiago099 <thiago.kaique01@gmail.com>
2022-05-06 16:23:52 -0300

    [feature] progress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {  ref } from 'vue'
 import { connection } from '@/global/mysql'
+
 const databases = ref([])
 const tables = ref([])
 
 const database = ref(null)
 const table = ref(null)
 
-const foreign_keys = ref([])
+const foreign_keys = ref<foreign_key_data[]>([])
 
-connection.query(`SELECT 
-KEY_COLUMN_USAGE.TABLE_NAME,
-KEY_COLUMN_USAGE.COLUMN_NAME,
-KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME,
-KEY_COLUMN_USAGE.REFERENCED_COLUMN_NAME
-FROM information_schema.TABLE_CONSTRAINTS
-INNER JOIN information_schema.KEY_COLUMN_USAGE ON TABLE_CONSTRAINTS.CONSTRAINT_NAME = KEY_COLUMN_USAGE.CONSTRAINT_NAME
-WHERE TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
-AND TABLE_CONSTRAINTS.TABLE_SCHEMA = DATABASE()`, (err, rows) => {
-  if (err) {
-    console.log(err)
-  } else {
-    foreign_keys.value = rows
-  }
-})
+interface foreign_key_data {
+  TABLE_NAME: string,
+  COLUMN_NAME: string,
+  REFERENCED_TABLE_NAME: string
+  REFERENCED_COLUMN_NAME: string,
+}
 
 connection.query('SHOW DATABASES', (err:any, results:any ) => {
   databases.value = results
 })
 
-function updateDatabase(event:any)
+function update()
 {
   if(database.value != null)
   {
@@ -38,14 +30,31 @@ function updateDatabase(event:any)
       const key = Object.keys(results[0])[0]
       tables.value = results.map((result:any) => result[key])
     })
+    connection.query(`SELECT 
+    KEY_COLUMN_USAGE.TABLE_NAME,
+    KEY_COLUMN_USAGE.COLUMN_NAME,
+    KEY_COLUMN_USAGE.REFERENCED_TABLE_NAME,
+    KEY_COLUMN_USAGE.REFERENCED_COLUMN_NAME
+    FROM information_schema.TABLE_CONSTRAINTS
+    INNER JOIN information_schema.KEY_COLUMN_USAGE ON TABLE_CONSTRAINTS.CONSTRAINT_NAME = KEY_COLUMN_USAGE.CONSTRAINT_NAME
+    WHERE TABLE_CONSTRAINTS.CONSTRAINT_TYPE = 'FOREIGN KEY'
+    AND TABLE_CONSTRAINTS.TABLE_SCHEMA = '${database.value}'`, (err, rows) => {
+      if (err) {
+        console.log(err)
+      } else {
+        foreign_keys.value = rows
+      }
+    })
   }
 }
-updateDatabase()
-// connection.query('use meaning')
-// connection.query('SHOW TABLES', (err:any, results:any ) => {
-//   const key = Object.keys(results[0])[0]
-//   databases.value = results.map((item:any)=> item[key])
-// })
+update()
+
+const fields = ref<foreign_key_data[]>([])
+function update_fields()
+{
+  fields.value = foreign_keys.value.filter((item :foreign_key_data) => item.TABLE_NAME == table.value)
+}
+
 </script>
 
 <template>
@@ -53,18 +62,34 @@ updateDatabase()
     <div class="row">
       <div class="col-6 form-group">
         <label for="">Databases</label>
-        <select name="" id="" v-model="database" class="form-select" @change="updateDatabase($event)">
+        <select name="" id="" v-model="database" class="form-select" @change="update()">
           <option :value="Database" v-for="({Database}) in databases" :key="Database"> {{ Database }} </option>
         </select>
       </div>
 
       <div class="col-6 form-group">
         <label for="">Tables</label>
-        <select name="" id="" class="form-select" v-model="table">
+        <select name="" id="" class="form-select" v-model="table" @change="update_fields()">
           <option :value="table" v-for="table in tables" :key="table"> {{ table }} </option>
         </select>
       </div>
     </div>
+    <div class="row content-data">
+      <div class=" col-6">
+        <div class="form-control">
+          {{table}}
+          {{fields}}
+        </div>
+      </div>
+      <div class=" col-6">
+        <div class="form-control"></div>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.content-data{
+  margin-top: 10px;
+}
+</style>
 

a6da9b8049b1e74297d964e02f0c5eb1a8ece124
Thiago099 <thiago.kaique01@gmail.com>
2022-05-09 10:33:16 -0300

    [fix] style

--- src/app/App.vue
+++ src/app/App.vue
@@ -21,64 +21,79 @@ function set_selected(e:any, objects:any)
 
 const filter = ref('')
 
+const tab = ref('Join')
+const tabs = ['Join', 'Fields', 'Result']
+
 </script>
 
 <template>
-  <div class="container" >
-    <div class="row">
-      <div class="col-6 form-group">
-        <label for="">Databases</label>
-        <div class="input-group">
-          <select name="" id="" v-model="database" class="form-select" @change="update()">
-            <option :value="Database" v-for="({Database}) in databases" :key="Database"> {{ Database }} </option>
-          </select>
-            <button class="btn btn-primary" @click="update_databases();update();database=null;table=null"><i class="fa fa-rotate-left" ></i></button>
+  <div class="container">
+    <ul class="nav nav-tabs">
+      <li class="nav-item" v-for="tab_link in tabs" :key="tab_link">
+        <a class="nav-link" :class="{'active': tab_link == tab}" aria-current="page" href="#" @click="tab = tab_link">{{tab_link}}</a>
+      </li>
+    </ul>
+    <div v-show="tab == 'Join'">
+      <div class="row">
+        <div class="col-6 form-group">
+          <label for="">Databases</label>
+          <div class="input-group">
+            <select name="" id="" v-model="database" class="form-select" @change="update()">
+              <option :value="Database" v-for="({Database}) in databases" :key="Database"> {{ Database }} </option>
+            </select>
+              <button class="btn btn-primary" @click="update_databases();update();database=null;table=null"><i class="fa fa-rotate-left" ></i></button>
+          </div>
+        </div>
+        <div class="col-6 form-group">
+          <label for="">Tables</label>
+          <div class="input-group">
+            <select name="" id="" class="form-select" v-model="table" @change="update_fields();">
+              <option :value="table" v-for="table in tables" :key="table"> {{ table }} </option>
+            </select>
+            <button class="btn btn-primary" @click="update();table=null"><i class="fa fa-rotate-left" ></i></button>
+          </div>
         </div>
       </div>
-      <div class="col-6 form-group">
-        <label for="">Tables</label>
-        <div class="input-group">
-          <select name="" id="" class="form-select" v-model="table" @change="update_fields();">
-            <option :value="table" v-for="table in tables" :key="table"> {{ table }} </option>
-          </select>
-          <button class="btn btn-primary" @click="update();table=null"><i class="fa fa-rotate-left" ></i></button>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-4">
-        <field-container
-          :active="false"
-          @child="move($event, 'child')"
-          @parent="move($event, 'parent')"
-          @update="update_query()"
-          :fields="fields"
-        />
-      </div>
-      <div class="col-8">
+      <div class="row">
+        <div class="col-4">
           <field-container
-            :active="true"
-
-            @child="back($event, 'child')"
-            @parent="back($event, 'parent')"
-            :fields="active_fields"
+            style="height:70vh"
+            :active="false"
+            @child="move($event, 'child')"
+            @parent="move($event, 'parent')"
             @update="update_query()"
+            :fields="fields"
           />
+        </div>
+        <div class="col-8">
+            <field-container
+              :active="true"
+              style="height:70vh"
+              @child="back($event, 'child')"
+              @parent="back($event, 'parent')"
+              :fields="active_fields"
+              @update="update_query()"
+            />
+        </div>
       </div>
     </div>
-    <i class="fa fa-search" style="position:absolute;margin-top:20px;margin-left:15px"></i>
-    <input type="text" class="form-control" v-model="filter" style="padding-left:40px">
-    <div class="content-data form-area form-control">
-      <div v-for="({table, columns}) of table_fields" :key="table">
-      <input type="checkbox" :checked="columns.every((item) => item.selected == true)" @click="set_selected($event, columns)"> {{table?.object?.alias ?? table.name}}
-        <div v-for="(column) of columns.filter(item=>item.name.includes(filter))" :key="column.name">
-          &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" v-model="column.selected" @change="update_query()"> {{column.name}}
-        </div >
+    <div v-show="tab == 'Fields'">
+      <i class="fa fa-search" style="position:absolute;margin-top:10px;margin-left:15px"></i>
+      <input type="text" class="form-control" v-model="filter" style="padding-left:40px">
+      <div class="content-data form-area form-control" style="height:70vh">
+        <div v-for="({table, columns}) of table_fields" :key="table" >
+        <input type="checkbox" :checked="columns.every((item) => item.selected == true)" @click="set_selected($event, columns)"> {{table?.object?.alias ?? table.name}}
+          <div v-for="(column) of columns.filter(item=>item.name.includes(filter))" :key="column.name">
+            &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" v-model="column.selected" @change="update_query()"> {{column.name}}
+          </div >
+        </div>
       </div>
     </div>
-    <div class="row content-data form-control">
-      <div class="col">
-        <div class="form-area" v-html="query">
+    <div v-show="tab == 'Result'">
+      <div class="row content-data form-control" style="height:85vh">
+        <div class="col">
+          <div class="form-area" v-html="query">
+          </div>
         </div>
       </div>
     </div>
@@ -89,7 +104,6 @@ const filter = ref('')
   margin-top: 10px !important;
 }
 .content-data{
-  height: 26vh;
   display:block;
   overflow-y: auto;
   margin-top: 10px;
